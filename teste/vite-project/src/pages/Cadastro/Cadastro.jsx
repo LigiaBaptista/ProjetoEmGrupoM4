0704571d3296f359fe85b59106ac97d222493025
Thiago Soares <106043536+ThiagoMSo@users.noreply.github.com>
2023-03-17 16:16:36 -0300

    Adicional do Thi

--- teste/vite-project/src/pages/Cadastro/Cadastro.jsx
+++ teste/vite-project/src/pages/Cadastro/Cadastro.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import Formulario from '../../components/Formulario/Formulario';
-import Header from "../../components/Header/Header.jsx";
-import Footer from "../../components/Footer/Footer.jsx";
+
 function Cadastro(){
     return(
         <Formulario/>
