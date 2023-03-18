f6804ddf572de22ce0fbee9d1784060f2aafc76e
zViniicius <vncs2009@gmail.com>
2023-03-18 18:03:05 -0300

    Login page Complete

--- teste/vite-project/src/pages/Login/Login.jsx
+++ teste/vite-project/src/pages/Login/Login.jsx
@@ -1,13 +1,16 @@
 import { Button, Form, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/img/logo.png';
 
 function Login() {
   return (
-    <Container className="d-flex flex-column justify-content-center align-items-center bg-success" style={{maxWidth: 400}}>
+    <Container
+      className="d-flex  flex-column justify-content-center align-items-center btn-flat rounded p-4 my-5 shadow text-white"
+      style={{ maxWidth: 400, background: '#101010' }}>
       <img src={logo} width={350} alt="logo" />
       <Form className="w-100">
-        <Form.Group className="" controlId="exampleForm.ControlInput1">
-          <Form.Label>Email address</Form.Label>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+          <Form.Label>Email</Form.Label>
           <Form.Control type="email" placeholder="Insira seu email" autoFocus />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
@@ -18,6 +21,9 @@ function Login() {
           Entrar
         </Button>
       </Form>
+      <p className="mt-3 mb-0">
+        Não tem uma conta? <Link to="/cadastro">Cadastre-se</Link>
+      </p>
     </Container>
   );
 }
