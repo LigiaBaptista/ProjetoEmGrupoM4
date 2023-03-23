d9621eb65cf904621e3be5ca7c56c6bcbaee2da1
LigiaBaptista <ligiamarias_baptista@hotmail.com>
2023-03-23 00:41:03 -0300

    não mudei nada

--- teste/vite-project/src/main.jsx
+++ teste/vite-project/src/main.jsx
@@ -5,6 +5,8 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App'
+
+
 import './index.css';
 
 
